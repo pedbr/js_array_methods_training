563b60adb8841f227284e4aded216042e96cee0c
pedro_bras <pedro.c.bras@gmail.com>
2019-06-26 17:06:29 +0200

    testing with reduce

--- main.js
+++ main.js
@@ -15,85 +15,91 @@ const companies= [
 // FOR EACH 
 // we just want to console.log the array, or a value like the name
 // normal for loop like it was used before, now considered old practice
-//
-// for(let i = 0; i < companies.length; i++) {
-//     console.log(companies[i]);
-// }
-//
+for(let i = 0; i < companies.length; i++) {
+    console.log(companies[i]);
+}
+
 // with the forEach the solution is way more elegant
-//
-// forEach
-// companies.forEach(function(company) {
-//     console.log(company.name);
-// });
+forEach
+companies.forEach(function(company) {
+    console.log(company.name);
+});
 
 
 // FILTER
 // on the ages array we want to get 21 and older
 // with for loop we had to do the following
-//
-// let canDrink = [];
-// for(let i = 0; i < ages.length; i++) {
-//     if(ages[i] >= 21) {
-//         canDrink.push(ages[i]);
-//     }
-// }
-// console.log(canDrink);
-//
+let canDrink = [];
+for(let i = 0; i < ages.length; i++) {
+    if(ages[i] >= 21) {
+        canDrink.push(ages[i]);
+    }
+}
+console.log(canDrink);
+
 // with the filter loop again simpler and more elegant
-// const canDrink = ages.filter(function(age) {
-//     if(age >= 21) {
-//         return true;;
-//     }
-// });
-// console.log(canDrink)
-//
+const canDrink = ages.filter(function(age) {
+    if(age >= 21) {
+        return true;;
+    }
+});
+console.log(canDrink)
+
 // we can do it even more elegant with an arrow funtion
-// const canDrink = ages.filter(age => age >= 21);
-// console.log(canDrink);
-//
+const canDrink = ages.filter(age => age >= 21);
+console.log(canDrink);
+
 // if we want to find just retail companies
-// const retailCias = companies.filter(function(company){
-//     if (company.category === 'Retail'){
-//         return true;
-//     }
-// });
+const retailCias = companies.filter(function(company){
+    if (company.category === 'Retail'){
+        return true;
+    }
+});
 // or
-// const retailCias = companies.filter(company => company.category === 'Retail')
-// console.log(retailCias)
+const retailCias = companies.filter(company => company.category === 'Retail')
+console.log(retailCias)
 
 
 // MAP
 // we want to create an array of company names
-// const companyNames = companies.map(function(company){
-//     return company.name;
-// });
-// console.log(companyNames)
-//
+const companyNames = companies.map(function(company){
+    return company.name;
+});
+console.log(companyNames)
+
 // with arrow function
-// const companyNames = companies.map(company => company.name);
-//
-// console.log(companyNames)
+const companyNames = companies.map(company => company.name);
+
+console.log(companyNames)
 
 
 // SORT
 // sort companies by start year
-// const sortedCompanies = companies.sort(function(a, b){
-//     if(a.start > b.start) {
-//         return 1;
-//     } else {
-//         return -1;
-//     }
-// });
-// console.log(sortedCompanies)
-//
+const sortedCompanies = companies.sort(function(a, b){
+    if(a.start > b.start) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
+console.log(sortedCompanies)
+
 // or with arrow function
-// const sortedCompanies = companies.sort((a,b) => (a.start > b.start ? 1 : -1));
-// console.log(sortedCompanies)
-//
+const sortedCompanies = companies.sort((a,b) => (a.start > b.start ? 1 : -1));
+console.log(sortedCompanies)
+
 // to sort ages
-// const sortAges = ages.sort((a, b) => a - b);
-// console.log(sortAges)
+const sortAges = ages.sort((a, b) => a - b);
+console.log(sortAges)
 
 
 // REDUCE
+// to get the sum of all the ages
+const ageSum = ages.reduce(function(total, age) {
+    return total + age;
+}, 0);
+console.log(ageSum)
+
+// with arrow method
+const ageSum = ages.reduce((total, age) => total + age, 0);
+console.log(ageSum)
