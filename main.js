65527fb610eecafa22ea0857ed8a8eab2e5e6c00
pedro_bras <pedro.c.bras@gmail.com>
2019-06-26 16:13:46 +0200

    testing with foreach loop

--- main.js
+++ main.js
@@ -13,11 +13,17 @@ const companies= [
   const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // normal for loop like it was used before, now considered old practice
-for(let i = 0; i < companies.length; i++) {
-    console.log(companies[i]);
-}
+// for(let i = 0; i < companies.length; i++) {
+//     console.log(companies[i]);
+// }
 
   // forEach
+  
+  companies.forEach(function(company) {
+      console.log(company.name);
+  });
+
+
   // filter
   // map
   // sort
